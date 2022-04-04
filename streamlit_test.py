146a5b0de8f8c3ead1c34149552476a3a77ef7a6
Antoine Dusséaux <a455bcd9@users.noreply.github.com>
2022-04-04 14:21:02 +0100

    Create streamlit_test.py

---
+++ streamlit_test.py
@@ -0,0 +1,24 @@
+"""Display a something using Streamlit
+"""
+
+import json
+import requests
+import csv
+from datetime import date
+import os
+from pyairtable import Table
+from datetime import datetime, timezone
+from urllib.parse import urlparse
+import re
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Crypto-demographics of Courtyard NFT holders')
+data_load_state = st.text('Loading data...')
+
+AIRTABLE_KEY = os.getenv('AIRTABLE_KEY')
+AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
+table = Table(AIRTABLE_KEY, AIRTABLE_BASE_ID, 'users)
+              
+data_load_state.text("Done!")
